3b389b26ab98ab5467171c6c67ee3ab522e39cc8
cheima mezdour <jc_mezdour@esi.dz>
2023-11-18 13:47:43 +0100

    app.js updates

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -7,9 +7,38 @@ const Silat = () => {
     height: '100%',
     backgroundImage: 'url("/pics/bg_pic2.png")',
     backgroundPosition: 'center', // Adjust as needed
+    zIndex: 1, // Set a lower z-index
 
   };
 
+  const mainTextStyle = {
+    fontFamily: 'GE Jarida',
+    fontSize: '60px',
+    fontWeight: '900',
+    lineHeight: '75px',
+    letterSpacing: '0em',
+    textAlign: 'center',
+    color: '#078567',
+  };
+
+  const textStyle = {
+    fontFamily: 'Tajawal',
+    fontSize: '20px',
+    fontWeight: '400',
+    lineHeight: '24px',
+    letterSpacing: '0em',
+    textAlign: 'center',
+    marginTop: '20px',
+  };
+
+  const boxStyle = {
+    width: '1072.99px',
+    height: '328.69px',
+    border: '1px solid #049C78',
+    borderRadius: '50px',
+    bottom: '0', // Adjusted to stay at the bottom
+    left: '50%', // Adjusted to center the box
+  };
  
   
 
@@ -23,8 +52,15 @@ const Silat = () => {
 
   return (
     <div className="random-topics-wrapper" style={containerStyle}>
-      
-      
+      <div style={mainTextStyle}>
+        تعرّف على صِلَة
+      </div>
+      <div style={textStyle}>
+        {arabicText.split('\n').map((line, index) => (
+          <p key={index}>{line}</p>
+        ))}
+      </div>
+      <div style={boxStyle}></div>
     </div>
   );
 };
